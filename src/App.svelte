bffd0eef54003494e488627c2e26510a84ad298e
Kristof Gilicze <giliczekristof@gmail.com>
2025-01-16 11:58:05 +0100

    Add initial Svelte app structure with Poker game logic and Tailwind CSS integration

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import Css from './Css.svelte';
+
+//import Css from './Css.svelte';
+
+import { PokerGame } from './game/game'
+import { PokerState } from './game/PokerState.svelte';
+import PokerTable from './lib/PokerTable.svelte';
+
+const PLAYER_COUNT = 4;
+
+const state = new PokerState(PLAYER_COUNT);
+
+const game = new PokerGame(state);
+
+onMount(async () => {
+  game.shuffleDeck();
+
+  await game.dealCards();
+
+  game.logHands();
+  game.evaluate();
+})
+</script>
+
+<main>
+  <PokerTable state={state} />
+</main>
